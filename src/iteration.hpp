0fc970a01b35f5efc20c398b9c4fade5f16e0a54
Marco Nikander <m4n1k.developer@gmail.com>
2024-10-15 22:29:36 +0200

    template result type of the for_loop_object

--- src/iteration.hpp
+++ src/iteration.hpp
@@ -4,16 +4,17 @@
 
 namespace smp {
 
+template <typename T>
 struct for_loop_object
 {
-    for_loop_object(int first, int last) : _i{first}, _last{last} {}
+    for_loop_object(int first, int last, T result) : _i{first}, _last{last}, _result{result} {}
 
     int operator++() { return ++_i; }
     bool keep_going() const { return _i != _last; }
 
     int _i;
     int const _last;
-    std::string _result{""};
+    T _result;
 };
 
 
--- test/test_iteration.cpp
+++ test/test_iteration.cpp
@@ -5,12 +5,12 @@
 
 std::string concatenate_value(int const i, std::string result)
 {
-    return result + std::to_string(i) + " ";
+    return result + std::to_string(i) + std::string(" ");
 };
 
 TEST(iterate, nothing)
 {
-    smp::for_loop_object zero{0, 0};
+    smp::for_loop_object zero{0, 0, std::string{""}};
     auto const iteration_result = smp::iterate(zero, concatenate_value);
 
     EXPECT_EQ(iteration_result, std::string(""));
@@ -18,7 +18,7 @@ TEST(iterate, nothing)
 
 TEST(iterate, five)
 {
-    smp::for_loop_object zeroToFive{0, 5};
+    smp::for_loop_object zeroToFive{0, 5, std::string{""}};
     auto const iteration_result = smp::iterate(zeroToFive, concatenate_value);
 
     EXPECT_EQ(iteration_result, std::string("0 1 2 3 4 "));
