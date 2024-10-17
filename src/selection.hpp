e29ac0913d56b8e6f1fc2925f84912f80b6e49a9
Marco Nikander <m4n1k.developer@gmail.com>
2024-10-17 17:51:59 +0200

    simplify pseudocode for selection and sequence

--- src/selection.hpp
+++ src/selection.hpp
@@ -5,14 +5,7 @@ namespace msp {
 template <typename Condition, typename BlockTrue, typename BlockFalse>
 constexpr auto select(Condition condition, BlockTrue block_true, BlockFalse block_false)
 {
-    if(condition)
-    {
-        return block_true();
-    }
-    else
-    {
-        return block_false();
-    }
+    return condition ? block_true() : block_false();
 }
 
 }
--- src/sequence.hpp
+++ src/sequence.hpp
@@ -5,9 +5,7 @@ namespace msp {
 template <typename BlockFirst, typename BlockSecond>
 constexpr auto sequence(BlockFirst block_first, BlockSecond block_second)
 {
-    // I will have to unpack this pair and do something with the two results at
-    // the call-site of sequence :S
-    return std::make_pair(block_first(), block_second());
+    return block_second(block_first());
 }
 
 }
