d39a22e97e82ec09047753ac5139bf5e3cc15861
Marco Nikander <m4n1k.developer@gmail.com>
2024-10-17 18:25:51 +0200

    greatly simplify 'iterate'

--- src/iteration.hpp
+++ src/iteration.hpp
@@ -4,31 +4,18 @@
 
 namespace msp {
 
-template <typename T>
-struct for_loop_object
+template <typename Condition, typename Block>
+constexpr auto iterate(Condition condition, Block block)
 {
-    for_loop_object(int first, int last, T result) : _i{first}, _last{last}, _result{result} {}
-
-    int operator++() { return ++_i; }
-    int state() { return _i; }
-    bool keep_going() const { return _i != _last; }
-    T result() const { return _result; }
-
-    int _i;
-    int const _last;
-    T _result;
-};
-
-
-template <typename LoopCondition, typename Block>
-constexpr auto iterate(LoopCondition loop, Block block)
-{
-    while (loop.keep_going())
+    int i = 0;
+    auto result = typename Block::ResultType{};
+    
+    while (condition(i, result))
     {
-        loop._result = block(loop);
-        ++loop;
+        result = block(i, result);
+        ++i;
     }
-    return loop._result;
+    return result;
 }
 
 }
--- test/test_iteration.cpp
+++ test/test_iteration.cpp
@@ -3,26 +3,42 @@
 
 #include "../src/iteration.hpp"
 
-struct catenate_value {
-    template <typename LoopObject>
-    std::string operator()(LoopObject loop)
+struct string_catenate
+{
+    using ResultType = std::string;
+
+    std::string operator()(int i, std::string result)
     {
-        return loop.result() + std::to_string(loop.state()) + std::string(" ");
-    };
+        return std::move(result) + std::to_string(i) + std::string(" ");
+    }
+};
+
+struct less_than
+{
+    using ResultType = bool;
+
+    explicit less_than(int limit) : _limit{limit} {}
+    
+    template <typename T>
+    bool operator()(int i, T result) const
+    {
+        (void)result;
+        return i < _limit;
+    }
+
+    int const _limit;
 };
 
 TEST(iterate, nothing)
 {
-    msp::for_loop_object zero{0, 0, std::string{""}};
-    auto const iteration_result = msp::iterate(zero, catenate_value{});
+    auto const iteration_result = msp::iterate(less_than{0}, string_catenate{});
 
     EXPECT_EQ(iteration_result, std::string(""));
 }
 
 TEST(iterate, five)
 {
-    msp::for_loop_object zeroToFive{0, 5, std::string{""}};
-    auto const iteration_result = msp::iterate(zeroToFive, catenate_value{});
+    auto const iteration_result = msp::iterate(less_than{5}, string_catenate{});
 
     EXPECT_EQ(iteration_result, std::string("0 1 2 3 4 "));
 }
