b1240c97f4ee2df105d0c96745151aca60b4105f
Marco Nikander <m4n1k.developer@gmail.com>
2024-10-17 19:50:49 +0200

    implement and test 'sequence'

--- src/sequence.hpp
+++ src/sequence.hpp
@@ -2,10 +2,24 @@
 
 namespace msp {
 
-template <typename BlockFirst, typename BlockSecond>
-constexpr auto sequence(BlockFirst block_first, BlockSecond block_second)
+template <typename Glue, typename BlockFirst, typename BlockSecond>
+struct sequence
 {
-    return block_second(block_first());
-}
+    using ResultType = decltype(Glue{}(typename BlockFirst::ResultType{}, typename BlockSecond::ResultType{}));
+
+    sequence(Glue glue, BlockFirst block_first, BlockSecond block_second)
+        : _glue{glue}, _block_first{block_first}, _block_second{block_second} {}
+
+    template <typename InputType>
+    ResultType operator()(int i, InputType input) const
+    {
+        return _glue(_block_first(i, input), _block_second(i, input));
+    }
+
+    Glue _glue;
+    BlockFirst _block_first;
+    BlockSecond _block_second;
+};
+
 
 }
--- test/test_sequence.cpp
+++ test/test_sequence.cpp
@@ -1,3 +1,33 @@
 #include <gtest/gtest.h>
+#include <functional>
+#include <utility>
 
+#include "../src/example_functions.hpp"
 #include "../src/sequence.hpp"
+
+TEST(sequence, nothing)
+{
+    using namespace sample;
+
+    int const result = msp::sequence{std::plus<>{}, default_construct<int>{}, default_construct<int>{}}(2, 4);
+
+    EXPECT_EQ(result, 0);
+}
+
+TEST(sequence, add)
+{
+    using namespace sample;
+
+    int const result = msp::sequence{std::plus<>{}, get_result<int>{}, get_result<int>{}}(0, 42);
+
+    EXPECT_EQ(result, 84);
+}
+
+TEST(sequence, pair)
+{
+    using namespace sample;
+
+    auto const result = msp::sequence{std::make_pair<int, int>, default_construct<int>{}, default_construct<int>{}}(2, 4);
+
+    EXPECT_EQ(result, std::make_pair(0, 0));
+}
