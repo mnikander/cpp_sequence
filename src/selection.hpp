6a450499913cdfe4cdb79a4d856786f1ca039184
Marco Nikander <m4n1k.developer@gmail.com>
2024-10-17 19:23:52 +0200

    implement function object 'select' and its first unit test

--- src/selection.hpp
+++ src/selection.hpp
@@ -1,11 +1,28 @@
 #pragma once
 
+#include <type_traits> // is_same
+
 namespace msp {
 
 template <typename Condition, typename BlockTrue, typename BlockFalse>
-constexpr auto select(Condition condition, BlockTrue block_true, BlockFalse block_false)
-{
-    return condition ? block_true() : block_false();
-}
+struct select {
+    
+    static_assert(std::is_same<typename BlockTrue::ResultType, typename BlockFalse::ResultType>(),
+                  "BlockTrue and BlockFalse must have the same ResultType");
+    using ResultType = typename BlockTrue::ResultType;
+
+    select(Condition condition, BlockTrue block_true, BlockFalse block_false)
+        : _condition{condition}, _block_true{block_true}, _block_false{block_false} {}
+
+    template <typename InputType>
+    ResultType operator()(int i, InputType input) const
+    {
+        return _condition(i, input) ? _block_true(i, input) : _block_false(i, input);
+    }
+
+    Condition _condition;
+    BlockTrue _block_true;
+    BlockFalse _block_false;
+};
 
 }
--- test/test_selection.cpp
+++ test/test_selection.cpp
@@ -1,3 +1,13 @@
 #include <gtest/gtest.h>
 
+#include "../src/example_functions.hpp"
 #include "../src/selection.hpp"
+
+TEST(select, nothing)
+{
+    using namespace sample;
+    
+    std::string const result = msp::select{less_than{0}, string_catenate{}, default_construct<std::string>{}}(0, std::string{""});
+
+    EXPECT_EQ(result, "");
+}
