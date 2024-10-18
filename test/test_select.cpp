6905fe54728db7dde74563f5f75f81fa6ce52856
Marco Nikander <m4n1k.developer@gmail.com>
2024-10-18 12:39:15 +0200

    test 'select' on a container

--- test/test_select.cpp
+++ test/test_select.cpp
@@ -1,8 +1,12 @@
 #include <gtest/gtest.h>
+#include <functional>
+#include <string>
+#include <vector>
 
 #include "../src/example_functions.hpp"
 #include "../src/example_predicates.hpp"
 #include "../src/select.hpp"
+#include "../src/sequence.hpp"
 
 TEST(select, nothing)
 {
@@ -20,4 +24,25 @@ TEST(select, one)
     std::string const result = msp::select{equal_to{1}, string_catenate{}, default_construct<std::string>{}}(1, std::string{""});
 
     EXPECT_EQ(result, "1 ");
+}
+
+TEST(select, container)
+{
+    using namespace sample;
+
+    std::vector<int> const vec{13, 26, 42};
+
+    auto getter               = get{vec};
+    auto answer               = constant{42};
+    auto glue                 = std::equal_to<int>{};
+    auto yea                  = constant{std::string("Yes")};
+    auto nay                  = constant{std::string("No")};
+    auto const f              = msp::select{msp::sequence(glue, getter, answer), yea, nay};
+    std::string const result0 = f(0, 0);
+    std::string const result1 = f(1, 0);
+    std::string const result2 = f(2, 0);
+
+    EXPECT_EQ(result0, std::string("No"));
+    EXPECT_EQ(result1, std::string("No"));
+    EXPECT_EQ(result2, std::string("Yes"));
 }
