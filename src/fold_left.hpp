3fccda58da65db9e8bd751fbfe91a3c14d5c01ba
Marco Nikander <m4n1k.developer@gmail.com>
2024-11-12 08:50:42 +0100

    refactor `fold_left` to have a stopping predicate instead of a running predicate, which also ensures the accumulator is updated before the predicate is evaluated, which enables correct early-exit functionality

--- src/fold_left.hpp
+++ src/fold_left.hpp
@@ -3,11 +3,15 @@
 namespace fp {
 
 template <typename F, typename P, typename A, typename I>
-A fold_left(F function, P predicate, A accumulator, I current, I sentinel)
+A fold_left(F function, P stopping_predicate, A accumulator, I current, I sentinel)
 {
-    while (current != sentinel && predicate(accumulator, current, sentinel))
+    while (current != sentinel)
     {
-        accumulator = function(accumulator, current, sentinel);
+        accumulator     = function(accumulator, current, sentinel);
+        if(stopping_predicate(accumulator, current, sentinel))
+        {
+            return accumulator;
+        }
         ++current;
     }
     return accumulator;
--- test/test_fold_left.cpp
+++ test/test_fold_left.cpp
@@ -4,12 +4,12 @@
 #include "../src/global_datatypes.hpp"
 
 template <typename A, typename I>
-bool always_true(A accumulator, I current, I sentinel)
+bool always_false(A accumulator, I current, I sentinel)
 {
     (void)accumulator;
     (void)current;
     (void)sentinel;
-    return true;
+    return false;
 }
 
 template <typename A, typename I>
@@ -21,7 +21,7 @@ A plus(A accumulator, I current, I sentinel)
 
 TEST(fold_left, integer_sum)
 {
-    i32 result = fp::fold_left(plus<i32, i32>, always_true<i32, i32>, 0, 0, 5);
+    i32 result = fp::fold_left(plus<i32, i32>, always_false<i32, i32>, 0, 0, 5);
     EXPECT_EQ(result, 10);
 }
 
@@ -36,6 +36,6 @@ TEST(fold_left, container_sum)
 {
     using iter = typename vi32::const_iterator;
     vi32 V{1, 2, 4, 8, 16};
-    i32 result = fp::fold_left(plus_iter<i32, iter>, always_true<i32, iter>, 0, V.cbegin(), V.cend());
+    i32 result = fp::fold_left(plus_iter<i32, iter>, always_false<i32, iter>, 0, V.cbegin(), V.cend());
     EXPECT_EQ(result, 31);
 }
