067e49bb10bc95eefb33053a60b7334f2e33081a
Marco Nikander <m4n1k.developer@gmail.com>
2024-11-12 08:51:25 +0100

    implement and test `find` using the revised early-exit implementation of `left_fold`

---
+++ test/test_find.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+
+#include "../src/fold_left.hpp"
+#include "../src/global_datatypes.hpp"
+
+template <typename A, typename I>
+A return_current(A accumulator, I current, I sentinel)
+{
+    (void)accumulator;
+    (void)sentinel;
+    return current;
+}
+
+template <typename A, typename I>
+bool is_four(A accumulator, I current, I sentinel)
+{
+    (void)accumulator;
+    (void)sentinel;
+    return current == 4;
+}
+
+template <typename A, typename I>
+bool is_four_iter(A accumulator, I current, I sentinel)
+{
+    (void)accumulator;
+    (void)sentinel;
+    return *current == 4;
+}
+
+TEST(find, integer)
+{
+    i32 result = fp::fold_left(return_current<i32, i32>, is_four<i32, i32>, 0, 0, 6);
+    EXPECT_EQ(result, 4);
+}
+
+TEST(find, container)
+{
+    using iter = typename vi32::const_iterator;
+    vi32 V{1, 2, 4, 8, 16};
+    iter result = fp::fold_left(return_current<iter, iter>, is_four_iter<iter, iter>, V.cbegin(), V.cbegin(), V.cend());
+    
+    iter const expected = V.cbegin() + 2;
+    EXPECT_EQ(result, expected);
+}
