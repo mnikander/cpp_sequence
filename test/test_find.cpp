4619c89ff1763a1d76fb6c3c04153629104b7e96
Marco Nikander <m4n1k.developer@gmail.com>
2024-11-18 20:21:59 +0100

    expand unit tests for `find` and identify a failing edge case

--- test/test_find.cpp
+++ test/test_find.cpp
@@ -32,12 +32,41 @@ TEST(find, integer)
     EXPECT_EQ(result, 4);
 }
 
-TEST(find, container)
+TEST(find, container_empty)
+{
+    using iter = typename vi32::const_iterator;
+    vi32 V{};
+    iter result = fp::fold_left_if(return_current<iter, iter, iter>, is_four_iter<iter, iter, iter>, V.cbegin(), V.cbegin(), V.cend());
+
+    iter const expected = V.cend();
+    EXPECT_EQ(result, expected);
+}
+
+TEST(find, container_true)
 {
     using iter = typename vi32::const_iterator;
     vi32 V{1, 2, 4, 8, 16};
     iter result = fp::fold_left_if(return_current<iter, iter, iter>, is_four_iter<iter, iter, iter>, V.cbegin(), V.cbegin(), V.cend());
-    
+
     iter const expected = V.cbegin() + 2;
     EXPECT_EQ(result, expected);
+}
+
+TEST(find, container_false)
+{
+    using iter = typename vi32::const_iterator;
+    vi32 V{1, 2};
+    iter result = fp::fold_left_if(return_current<iter, iter, iter>, is_four_iter<iter, iter, iter>, V.cbegin(), V.cbegin(), V.cend());
+
+    iter const expected = V.cend();
+    EXPECT_EQ(result, expected);
+
+    // this unit test fails, because when `current == sentinel` the accumulator
+    // is no longer updated, so instead of returning `last` it returns one
+    // before the last.
+    
+    // This case is problematic, because I don't want to run the risk of
+    // dereferencing the past-the-end iterator. A`function` or a `predicate`
+    // which uses the sentinel value / iterator will segfault for many
+    // functions and predicates.
 }
