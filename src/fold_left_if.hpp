5b198c792844cbbb05c35a7501ffb4756946ad77
Marco Nikander <m4n1k.developer@gmail.com>
2024-11-18 19:55:57 +0100

    make comment more clear

--- src/fold_left_if.hpp
+++ src/fold_left_if.hpp
@@ -8,8 +8,9 @@ A fold_left_if(F function, P stopping_predicate, A accumulator, I current, S sen
     bool stop = false;
     while (current != sentinel && !stop)
     {
+        // note that the predicate uses the _updated_ accumulator value
         accumulator = function(accumulator, current, sentinel);
-        stop        = stopping_predicate(accumulator, current, sentinel); // note the updated accumulator value is used
+        stop        = stopping_predicate(accumulator, current, sentinel);
         ++current;
     }
     return accumulator;
