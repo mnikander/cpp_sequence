f40afd9a6d5c773f9d7531a2914daa6b64eb7fdf
Marco Nikander <m4n1k.developer@gmail.com>
2024-11-18 21:16:28 +0100

    simplify the functions in the `find` test case slightly

--- test/test_find.cpp
+++ test/test_find.cpp
@@ -2,20 +2,20 @@
 #include "../src/fold_left_if.hpp"
 #include "../src/global_datatypes.hpp"
 
-template <typename A, typename I, typename S>
-A assign_if_four(A accumulator, I current, S sentinel)
+template <typename I>
+I assign_if_four(I accumulator, I current, I sentinel)
 {
     (void)accumulator;
     (void)sentinel;
-    return current == 4 ? A{current} : accumulator;
+    return current == 4 ? current : accumulator;
 }
 
-template <typename A, typename I, typename S>
-A assign_if_four_iter(A accumulator, I current, S sentinel)
+template <typename I, typename S>
+I assign_if_four_iter(I accumulator, I current, S sentinel)
 {
     (void)accumulator;
     (void)sentinel;
-    return *current == 4 ? A{current} : accumulator;
+    return *current == 4 ? current : accumulator;
 }
 
 template <typename A, typename I, typename S>
@@ -27,7 +27,7 @@ bool is_found(A accumulator, I current, S sentinel)
 
 TEST(find, integer)
 {
-    i32 result = fp::fold_left_if(assign_if_four<i32, i32, i32>, is_found<i32, i32, i32>, 6, 0, 6);
+    i32 result = fp::fold_left_if(assign_if_four<i32>, is_found<i32, i32, i32>, 6, 0, 6);
     EXPECT_EQ(result, 4);
 }
 
@@ -35,7 +35,7 @@ TEST(find, container_empty)
 {
     using iter = typename vi32::const_iterator;
     vi32 V{};
-    iter result = fp::fold_left_if(assign_if_four_iter<iter, iter, iter>, is_found<iter, iter, iter>, V.cend(), V.cbegin(), V.cend());
+    iter result = fp::fold_left_if(assign_if_four_iter<iter, iter>, is_found<iter, iter, iter>, V.cend(), V.cbegin(), V.cend());
 
     iter const expected = V.cend();
     EXPECT_EQ(result, expected);
@@ -45,7 +45,7 @@ TEST(find, container_true)
 {
     using iter = typename vi32::const_iterator;
     vi32 V{1, 2, 4, 8, 16};
-    iter result = fp::fold_left_if(assign_if_four_iter<iter, iter, iter>, is_found<iter, iter, iter>, V.cend(), V.cbegin(), V.cend());
+    iter result = fp::fold_left_if(assign_if_four_iter<iter, iter>, is_found<iter, iter, iter>, V.cend(), V.cbegin(), V.cend());
 
     iter const expected = V.cbegin() + 2;
     EXPECT_EQ(result, expected);
@@ -55,7 +55,7 @@ TEST(find, container_false)
 {
     using iter = typename vi32::const_iterator;
     vi32 V{1, 2};
-    iter result = fp::fold_left_if(assign_if_four_iter<iter, iter, iter>, is_found<iter, iter, iter>, V.cend(), V.cbegin(), V.cend());
+    iter result = fp::fold_left_if(assign_if_four_iter<iter, iter>, is_found<iter, iter, iter>, V.cend(), V.cbegin(), V.cend());
 
     iter const expected = V.cend();
     EXPECT_EQ(result, expected);
