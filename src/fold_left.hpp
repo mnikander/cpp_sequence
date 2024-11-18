fcc90fbf4baabd5ff746918dac52eb9585960cc3
Marco Nikander <m4n1k.developer@gmail.com>
2024-11-18 20:37:18 +0100

    simplify `fold_left`

--- src/fold_left.hpp
+++ src/fold_left.hpp
@@ -5,13 +5,12 @@ namespace fp {
 template <typename F, typename P, typename A, typename I, typename S>
 A fold_left(F function, P stopping_predicate, A accumulator, I current, S sentinel)
 {
-    while (current != sentinel)
+    bool stop = false;
+
+    while (current != sentinel && !stop)
     {
         accumulator = function(accumulator, current, sentinel);
-        if(stopping_predicate(accumulator, current, sentinel))
-        {
-            return accumulator;
-        }
+        stop        = stopping_predicate(accumulator, current, sentinel); // note the updated accumulator value is used
         ++current;
     }
     return accumulator;
