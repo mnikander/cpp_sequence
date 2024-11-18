97820b13bc22a63d65201ac787c98a2e4277de84
Marco Nikander <m4n1k.developer@gmail.com>
2024-11-18 20:20:40 +0100

    rename stop to keep_going and invert the logic

--- src/fold_left_if.hpp
+++ src/fold_left_if.hpp
@@ -5,12 +5,12 @@ namespace fp {
 template <typename F, typename P, typename A, typename I, typename S>
 A fold_left_if(F function, P stopping_predicate, A accumulator, I current, S sentinel)
 {
-    bool stop = false;
-    while (current != sentinel && !stop)
+    bool keep_going = true;
+    while (current != sentinel && keep_going)
     {
         // note that the predicate uses the _updated_ accumulator value
         accumulator = function(accumulator, current, sentinel);
-        stop        = stopping_predicate(accumulator, current, sentinel);
+        keep_going  = !stopping_predicate(accumulator, current, sentinel);
         ++current;
     }
     return accumulator;
