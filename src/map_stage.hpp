2cf023f23b5a50dc379427aadb4eda301bbb282c
Marco Nikander <m4n1k.developer@gmail.com>
2025-02-18 09:55:08 +0100

    implement and test MapStage

---
+++ src/map_stage.hpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2025, Marco Nikander
+
+#pragma once
+#include <utility> // forward
+#include "stage.hpp"
+
+namespace seq {
+
+template <typename F>
+struct UnaryTransformation {
+
+    explicit UnaryTransformation(F unaryFunction) : _function{unaryFunction} {}
+
+    // We need a reference to the real instance of the emitter, held by the stage.
+    // Since this instance is only available AFTER the constructor of the stage has finished, we
+    // must pass it in every call. I am not sure if this has runtime performance repercussions.
+    // To find out, I would need to benchmark the 'Stage' against an equivalent 'MapStage'.
+    template <typename Emitter, typename T>
+    void operator()(Emitter& emit, T&& arg) {
+        emit(_function(std::forward<T>(arg)));
+    }
+
+    F _function;
+};
+
+template <typename T, typename F, typename S>
+using MapStage = Stage<T, UnaryTransformation<F>, S>;
+
+template <typename T, typename F, typename S>
+auto make_map_stage(F unaryFunction, S successor) {
+    return Stage<T, UnaryTransformation<F>, S>{UnaryTransformation{unaryFunction}, successor};
+}
+
+}
---
+++ test/map_stage.test.cpp
@@ -0,0 +1,55 @@
+// Copyright (c) 2025, Marco Nikander
+
+#include <array>
+#include <gtest/gtest.h>
+#include "../src/datatypes.hpp"
+#include "../src/emit.hpp"
+#include "../src/iota_generator.hpp"
+#include "../src/map_stage.hpp"
+#include "../src/range_sink.hpp"
+
+TEST(map_stage, identity)
+{
+    using namespace seq;
+    std::array<mutable_i64, 5> result{0, 0, 0, 0, 0};
+    std::array<mutable_i64, 5> const expected{0, 1, 2, 3, 4};
+
+    // pipeline stages, from last to first
+    auto sink     = make_range_sink(result);
+    auto square   = make_map_stage<i64>([](i64 i){ return i; }, sink);
+    auto sequence = make_iota_generator(square);
+
+    sequence.yield(5);
+    EXPECT_EQ(result, expected);
+}
+
+TEST(map_stage, square)
+{
+    using namespace seq;
+
+    std::array<mutable_i64, 5> result{0, 0, 0, 0, 0};
+    std::array<mutable_i64, 5> const expected{0, 1, 4, 9, 16};
+
+    // pipeline stages, from last to first
+    auto sink     = make_range_sink(result);
+    auto square   = make_map_stage<i64>([](i64 value){ return value*value; }, sink);
+    auto sequence = make_iota_generator(square);
+
+    sequence.yield(5);
+    EXPECT_EQ(result, expected);
+}
+
+TEST(map_stage, int_to_float)
+{
+    using namespace seq;
+    std::array<mutable_f64, 5> result{0.0, 0.0, 0.0, 0.0, 0.0};
+    std::array<mutable_f64, 5> const expected{0.0, 1.0, 2.0, 3.0, 4.0};
+
+    // pipeline stages, from last to first
+    auto sink     = make_range_sink(result);
+    auto square   = make_map_stage<i64>([](i64 i){ return static_cast<mutable_f64>(i); }, sink);
+    auto sequence = make_iota_generator(square);
+
+    sequence.yield(5);
+    EXPECT_EQ(result, expected);
+}
