10f16eac1b7acbf11a15ffd85c1388e0ec90a8ca
Marco Nikander <m4n1k.developer@gmail.com>
2025-02-22 16:03:20 +0100

    cleanup datatypes

--- src/datatypes.hpp
+++ src/datatypes.hpp
@@ -2,25 +2,23 @@
 #include <cstdint>
 #include <vector>
 
-// several useful datatypes which are const by default
-
 namespace seq {
 
 // primitive aliases
-using b8           = bool;
-using c8           = char;
-using i32          = int;
-using i64          = int64_t;
-using f32          = float;
-using f64          = double;
+using b8   = bool;
+using c8   = char;
+using i32  = int;
+using i64  = int64_t;
+using f32  = float;
+using f64  = double;
 
 // vector aliases
-using vb8          = std::vector<bool>;
-using vc8          = std::vector<char>;
-using vi32         = std::vector<int>;
-using vi64         = std::vector<int64_t>;
-using vf32         = std::vector<float>;
-using vf64         = std::vector<double>;
+using vb8  = std::vector<bool>;
+using vc8  = std::vector<char>;
+using vi32 = std::vector<int>;
+using vi64 = std::vector<int64_t>;
+using vf32 = std::vector<float>;
+using vf64 = std::vector<double>;
 
 // like an enum, but I really do want it to be a `bool` under the surface
 using Status = bool;
