f57d63eb9fea705c57fc93355ca75deccec1b8f0
Marco Nikander <m4n1k.developer@gmail.com>
2025-02-23 11:35:31 +0100

    document the generic stage a bit, for advanced users

--- src/stage/generic/stage.hpp
+++ src/stage/generic/stage.hpp
@@ -7,6 +7,15 @@
 
 namespace seq {
 
+// This is a generic implementation for a stage, intended for advanced users.
+// You can find a usage example in the corresponding unit test.
+// The idea is that you pass a 'transformation' function into the generic stage,
+// to define its behavior. This transformation function must take exactly two
+// arguments: an 'emit' function and the value to be processed. The emit
+// function is a wrapper around the call to '_successor.receive(value)`, which
+// allows you to send 0, 1, 2, or more computation results to the following
+// stages, via a corresponding number of calls to emit(value).
+
 template <typename T, typename F, typename S>
 struct Stage {
     using Input = T;
